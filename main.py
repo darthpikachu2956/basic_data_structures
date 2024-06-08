991546a65f4b8a05eca6b50a7f351a47222d3b34
Vlad <darthpikachu2956@gmail.com>
2024-06-09 00:20:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+a = 'aezakmi'
+print(a)
+print(len(a))
+# That was the task "Длина слова"
+
+first = 10
+second = 8
+summa = first+second
+diff = first-second
+print(summa, diff)
+# That was the task "Суммы и разности"
+
+third = 3
+mean = (first+second+third) // 3
+print(mean)
+# That was the task "Среднее арифметическое"
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+# That was the task "Простые строчки"
+
+a = 2
+b = 4
+c = 6
+f = (a*b)+(a*c)
+print(a, f, f**3)
+print(f**3 / 2)
+# That was the task "Сложная формула"
