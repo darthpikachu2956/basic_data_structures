230829fcabe1c248ea56e95000ea3e3c26bfd8c6
Vlad <darthpikachu2956@gmail.com>
2024-06-13 20:20:51 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -2,7 +2,6 @@ name = 'Vlad'
 print('Name:', name)
 age = 26
 print('Age:', age)
-new_age = age + 1
-print('New age:', new_age)
+print('New age:', age + 1)
 is_student = True
 print('Is student:', is_student)
