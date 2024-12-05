ac4d2c11ce728f720f3b6439319bc488043cc7a0
Vlad <darthpikachu2956@gmail.com>
2024-12-06 00:44:03 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,21 @@
+# 1st program
+print((9 ** 0.5) * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+a = 2 * 2 + 2
+print(a)
+b = 2 * (2 + 2)
+print(b)
+print(a == b)
+
+# 4th program
+c = '123.456'
+print(c[4])
+d = float(c)
+e = d * 10
+print(e)
+print(int(e))
+print(int(e % 10))
